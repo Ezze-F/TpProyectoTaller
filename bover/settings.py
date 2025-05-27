6ca05f53e0f82433db76c8aa30f8214a91e96f75
Alexis <alexisromeroxd616@gmail.com>
2025-05-27 13:13:47 -0300

    colores de boton y linea divisora de productos

--- bover/settings.py
+++ bover/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'base_producto',
         'USER': 'root',
-        'PASSWORD': '191265',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
