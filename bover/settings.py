0be07d2796fd3f17e3ebe2b10106fe30d8eb17f7
Ezze <ezefarfan@gmail.com>
2025-06-19 20:35:17 -0300

    Modificaciones realizadas antes de la presentación

--- bover/settings.py
+++ bover/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'base_producto',
         'USER': 'root',
-        'PASSWORD': '191265',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- productos/forms.py
+++ productos/forms.py
@@ -5,6 +5,14 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Productos # el objeto model va a ser del tipo Productos (entidad que pertenece a models.py)
         fields = "__all__" # se van a utilizar todos los campos
+        labels = { # etiquetas para que se muestren en los modals
+            'preciounitario': 'Precio unitario',
+            'marca': 'Marca del producto',
+            'cantidad': 'Cantidad disponible',
+            'fechaelaboracion': 'Fecha de elaboración',
+            'fechavencimiento': 'Fecha de vencimiento',
+            'descripcion': 'Descripción del producto',
+        }
         exclude = ['codigo', 'is_deleted', 'deleted_at']  # campos excluidos
         widgets = {
             'fechaelaboracion': forms.DateInput(
